5dc9b8f5cad057d876cbffd7347dd830f76576d0
Gustavo Lourenço <gustavo.lourenco@dbccompany.com.br>
2022-11-10 17:17:10 -0300

    Monta estrutura inicial do desafio

---
+++ src/main/java/br/desafioRest/Constantes.java
@@ -0,0 +1,15 @@
+package br.desafioRest;
+
+import io.restassured.http.ContentType;
+
+public class Constantes {
+
+    static String APP_BASE_URL = "httpS://barrigarest.wcaquino.me";
+    static Integer APP_PORT = 443;
+    static String APP_BASE_PATH = "";
+
+    static ContentType APP_CONTENT_TYPE = ContentType.JSON;
+
+    static Long MAX_TIMEOUT = 1000L;
+
+}
